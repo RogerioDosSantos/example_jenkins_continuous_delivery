2fcca312b5956ae37b1b1bacecaa6ba1f0c14b49
Roger Santos <roger.santos.main@gmail.com>
2017-12-31 20:30:22 -0600

    Added C++ project

---
+++ build/clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Exit on any non-zero status.
+trap 'exit' ERR
+set -E
+
+# Get variables based on the configuration
+cd "$(dirname "$0")"
+bash_dir=$(pwd)
+
+echo " "
+echo "*** Cleanning: ${bash_dir}" 
+rm -r */
+rm -r ${bash_dir}/../stage/
---
+++ build/linux_default.cmake
@@ -0,0 +1,5 @@
+
+set(config_platform "${CMAKE_SYSTEM}")
+set(config_processor "${CMAKE_SYSTEM_PROCESSOR}")
+set(config_output_dir "${config_platform}/${config_processor}")
+message("- Config Output Directory: ${config_output_dir}")
